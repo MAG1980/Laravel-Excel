6b0b627ff3421249069d9e4e0f45ba0b1d7b4ecb
MAG1980 <al-1980@inbox.ru>
2026-03-19 16:47:40 +0300

    Реализовано сохранение загруженного файла в папку /storage.

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\Project\ImportStoreRequest;
+use App\Models\File;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class ProjectController extends Controller
 {
@@ -21,6 +23,14 @@ class ProjectController extends Controller
     public function importStore(ImportStoreRequest $request)
     {
         $data = $request->validated();
+        $file = $data['file'];
+        $path = Storage::disk('public')->put('files/', $data['file']);
+        File::create([
+            'title'=>$file->getClientOriginalName(),
+            'path'=>$path,
+            'mime_type'=>$file->getClientOriginalExtension(),
+
+        ]);
         dd($data);
     }
 }
