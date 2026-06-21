8bd21dacbf08fe27ad0a75109a6e1950852d9d39
MAG1980 <al-1980@inbox.ru>
2026-06-21 19:48:42 +0300

    Использованы имена маршрутов в router-link.

--- resources/js/spa/components/Navigation.vue
+++ resources/js/spa/components/Navigation.vue
@@ -1,11 +1,13 @@
 <template>
     <div id="app">
         <nav class="flex gap-4">
-            <router-link  to="/">Home</router-link>
-            <router-link to="/about">About</router-link>
-            <router-link to="/get">Get</router-link>
-            <router-link to="/user/login">Login</router-link>
-            <router-link to="/user/registration">Registration</router-link>
+            <router-link :to="{ name: 'home' }">Home</router-link>
+            <router-link :to="{ name: 'about' }">About</router-link>
+            <router-link :to="{ name: 'get.index' }">Get</router-link>
+            <router-link :to="{ name: 'user.login' }">Login</router-link>
+            <router-link :to="{ name: 'user.registration' }"
+                >Registration</router-link
+            >
         </nav>
     </div>
 </template>
