615a8904e51dfc41b86adbed055d7bdb462ae653
MAG1980 <al-1980@inbox.ru>
2026-07-13 11:18:28 +0300

    Добавлен скрипт для автоматизации deploy на хостинге.

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo "Deploying started..."
+
+git pull origin main
+
+php8.2 artisan down
+
+php8.2 composer.phar install --no-dev --optimize-autoloader
+
+php8.2 artisan migrate --force
+
+php8.2 artisan config:cache
+
+php8.2 artisan route:cache
+
+php8.2 artisan view:cache
+
+php8.2 artisan event:cache
+
+php8.2 artisan queue:restart
+
+php8.2 artisan up
+
+echo "Deploying completed"
+
+
+
